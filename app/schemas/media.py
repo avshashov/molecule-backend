885746455e28fb2556b9fd4cc76c5b8b11ada322
Artem Shashov <tauquantum@gmail.com>
2023-11-26 14:01:18 +1000

    fix: fixed attributes for media schema classes

--- app/schemas/media.py
+++ app/schemas/media.py
@@ -4,13 +4,13 @@ from pydantic import BaseModel
 class MediaBase(BaseModel):
     name: str
     type_id: int
-    link: str
     description: str
 
 
 class Media(MediaBase):
     id: int
+    link: str
 
 
 class MediaCreate(MediaBase):
-    pass
+    file: bytes
