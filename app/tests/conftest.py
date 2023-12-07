ed678090dab98d6a6fff4dd7fc7c2637568493c2
avshashov <tauquantum@gmail.com>
2023-12-07 21:32:31 +1000

    feat: BROKEN! updated test modules

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,10 +1,9 @@
-from contextlib import asynccontextmanager
+import asyncio
 from typing import AsyncGenerator
 
 import pytest
-from fastapi import FastAPI
 from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from app.database.models import Base
 from app.database.settings import Database
@@ -26,35 +25,23 @@ class TestDatabase(Database):
 _test_db = TestDatabase()
 
 
+@pytest.fixture(scope='session')
+async def db_session() -> AsyncSession:
+    async for session in _test_db.get_session():
+        yield session
+
+
+# app.dependency_overrides[Session] = db_session
+
+
 @pytest.fixture(autouse=True, scope='session')
-async def prepare_database():
+async def prepare_database() -> None:
     await _test_db.create_tables()
     yield
     await _test_db.drop_tables()
 
 
 @pytest.fixture(scope='session')
-async def async_client() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url='http://test') as ac:
-        yield ac
-
-
-# @asynccontextmanager
-# async def lifespan(app: FastAPI):
-#     await _test_db.create_tables()
-#     yield
-#     await _test_db.drop_tables()
-
-
-# @app.on_event("startup")
-# async def startup():
-#     await _test_db.create_tables()
-#
-#
-# @app.on_event("shutdown")
-# async def shutdown():
-#     await _test_db.drop_tables()
-
-app.dependency_overrides[Session] = _test_db.get_session
-
-# async_client = AsyncClient(app=app, base_url="http://test")
+async def async_client() -> AsyncClient:
+    async with AsyncClient(app=app, base_url='http://test') as client:
+        yield client
--- app/tests/test_press.py
+++ app/tests/test_press.py
@@ -1,33 +1,69 @@
 import datetime
 
+import pytest
+from fastapi import Depends
+from httpx import AsyncClient
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import models
-from app.tests.conftest import async_client, _test_db
+from app.tests.conftest import _test_db
 
 
-async def test_get_all_press():
-    async with async_client:
-        response = await async_client.get('/')
-        assert response.status_code == 404
+@pytest.mark.asyncio(scope='session')
+async def test_get_all_press(async_client: AsyncClient):
+    response = await async_client.get('/press/press_dates')
+    print(response.json())
+    assert response.status_code == 404
 
 
-async def test_add_press_to_db():
-    session = _test_db.get_session()
-    item = models.Press(
-        title='test',
-        description='test',
-        created_at=datetime.date(2023, 12, 7),
-        preview_photo_id=1,
-        external_link='link',
+@pytest.mark.asyncio(scope='session')
+async def test_create_press_article(async_client: AsyncClient, db_session: AsyncSession):
+    response = await async_client.post(
+        '/press/',
+        json={
+            'title': 'test21',
+            'description': 'test1',
+            'created_at': '2023-12-07',
+            'preview_photo_id': 2,
+            'external_link': 'link1',
+        },
     )
-    session.add(item)
-    await session.commit()
-    assert True
+    print(response.json())
+    assert response.status_code == 201
 
 
-async def test_get_item():
-    session = _test_db.get_session()
-    stmt = select(models.Press).where(models.Press.title == 'test')
-    item = await session.execute(stmt)
-    assert item.scalar() == ('test', 'test', datetime.date(2023, 12, 7), 1, 'link')
+# @pytest.mark.asyncio(scope='session')
+# async def test_add_press_to_db(db_session: AsyncSession):
+#     item1 = models.Press(
+#         title='test1',
+#         description='test1',
+#         created_at=datetime.date(2023, 12, 7),
+#         preview_photo_id=1,
+#         external_link='link1',
+#     )
+#     item2 = models.Press(
+#         title='test2',
+#         description='test2',
+#         created_at=datetime.date(2023, 10, 27),
+#         preview_photo_id=2,
+#         external_link='link2',
+#     )
+#     db_session.add_all([item1, item2])
+#     await db_session.commit()
+#     assert True
+
+
+@pytest.mark.asyncio(scope='session')
+async def test_get_all_press1(async_client: AsyncClient):
+    response = await async_client.get('/press/press_dates')
+    print(response.json())
+    assert response.status_code == 404
+
+
+# @pytest.mark.asyncio(scope='session')
+# async def test_get_item(db_session: AsyncSession):
+#     session = _test_db.get_session()
+#     stmt = select(models.Press).where(models.Press.title == 'test')
+#     item = await session.execute(stmt)
+#     assert item.scalar() == ('test', 'test', datetime.date(2023, 12, 7), 1, 'link')
