828a8c52574226a5ef7f62ca3a165a92f3f65c9d
avshashov <tauquantum@gmail.com>
2023-12-11 13:24:28 +1000

    feat: added schemas for projects

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.schemas.press import Press, PressUpdate, PressCreate, PressBase
 from app.schemas.news import News, NewsUpdate, NewsCreate, NewsBase
+from app.schemas.projects import Projects, ProjectsUpdate, ProjectsCreate, ProjectsBase
 from app.schemas.media import Media, MediaBase, MediaCreate
