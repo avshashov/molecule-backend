d729d965bbafbb5ace34383766aa93f4140b21e4
avshashov <tauquantum@gmail.com>
2023-12-13 19:27:34 +1000

    feat: added media_types endpoints and media/common CRUD

--- app/routers/media/api.py
+++ app/routers/media/api.py
@@ -7,10 +7,19 @@ from app.database import models
 from app.database.settings import database
 from app import schemas
 from app.routers.common import files
-from app.routers.common.crud import CRUD
+from app.routers.common.crud import CRUD as CommonCRUD
+from app.routers.media.crud import CRUD
 
 router = APIRouter(prefix='/media', tags=['media'])
 Session = Annotated[AsyncSession, Depends(database.get_session)]
+
+
+@router.get('/media_types')
+async def get_media_types(db: Session):
+    types = await CRUD.get_types(session=db)
+    if types:
+        return types
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'MediaTypes not found')
 
 
 @router.get('/')
@@ -19,7 +28,7 @@ async def get_media_files(
     count: Annotated[int, Query(ge=5, le=50)] = 10,
     offset: Annotated[int, Query(ge=0, le=50)] = 0,
 ):
-    media = await CRUD.get_media_items(session=db, count=count, offset=offset)
+    media = await CRUD.get_items(session=db, count=count, offset=offset)
     if media:
         return media
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Media not found')
@@ -27,7 +36,7 @@ async def get_media_files(
 
 @router.get('/{media_id}')
 async def get_media_file(db: Session, media_id: int):
-    media = await CRUD.get_media_items(session=db, id=media_id)
+    media = await CRUD.get_items(session=db, id=media_id)
     if media:
         return media
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Media with {media_id} not found')
@@ -45,22 +54,24 @@ async def upload_media_file(
     media_metadata = schemas.MediaCreate(
         name=media_name, type_id=media_type_id, link=filepath, description=media_description
     )
-    file_metadata = await CRUD.create_item(session=db, model=models.Media, schema_fields=media_metadata)
+    file_metadata = await CommonCRUD.create_item(
+        session=db, model=models.Media, schema_fields=media_metadata
+    )
     return file_metadata
 
 
 @router.delete('/{media_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_media_file(db: Session, media_id: int):
-    if not await CRUD.item_exists(session=db, id=media_id, model=models.Media):
+    if not await CommonCRUD.item_exists(session=db, id=media_id, model=models.Media):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Media with {media_id} not found')
-    await CRUD.delete_item(session=db, id=media_id, model=models.Media)
+    await CommonCRUD.delete_item(session=db, id=media_id, model=models.Media)
 
 
 @router.patch('/{media_id}', status_code=status.HTTP_200_OK)
 async def patch_name_or_description_media(db: Session, media_id: int, new_media_fields: schemas.MediaUpdate):
-    if not await CRUD.item_exists(session=db, id=media_id, model=models.Media):
+    if not await CommonCRUD.item_exists(session=db, id=media_id, model=models.Media):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Media with {media_id} not found')
-    media = await CRUD.update_item(
+    media = await CommonCRUD.update_item(
         session=db, id=media_id, model=models.Media, schema_fields=new_media_fields
     )
     return media
