70141b4cc821dff153be300612b12d802fd8d12b
avshashov <tauquantum@gmail.com>
2023-12-15 20:11:43 +1000

    rebase dev continue

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from app.schemas.press import Press, PressUpdate, PressCreate
 from app.schemas.news import News, NewsUpdate, NewsCreate, NewsMany
 from app.schemas.projects import Project, Projects, ProjectUpdate, ProjectCreate, ProjectCategories
-from app.schemas.media import Media, MediaCreate, MediaUpdate
+from app.schemas.media import Media, MediaCreate
 from app.schemas.dates import Dates
 from app.schemas.electrons import Electrons, ElectronsCreate, ElectronsUpdate
