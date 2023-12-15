dc3f687d0fc7c02b1be7cd830030cd82f489a520
avshashov <tauquantum@gmail.com>
2023-12-16 00:28:20 +1000

    refactor: isort

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from contextlib import asynccontextmanager
 import uvicorn
 from fastapi import FastAPI
 
-from app.routers.we.api import router as electrons
+from app.routers.media.api import router as media
+from app.routers.news.api import router as news
 from app.routers.press.api import router as press
 from app.routers.projects.api import router as projects
-from app.routers.news.api import router as news
-from app.routers.media.api import router as media
+from app.routers.we.api import router as electrons
 
 
 @asynccontextmanager
