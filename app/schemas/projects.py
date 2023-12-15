5b749d5b95d72d0045cd38b1f8bc6f0b64de21fc
avshashov <tauquantum@gmail.com>
2023-12-15 20:10:55 +1000

    feat: added new schemas for project categories, fix name and attributes for old schemas

--- app/schemas/projects.py
+++ app/schemas/projects.py
@@ -2,28 +2,45 @@ from datetime import date
 
 from pydantic import BaseModel
 
+from app.schemas.media import Media
 
-class ProjectsBase(BaseModel):
+
+class ProjectCategory(BaseModel):
+    id: int
+    name: str
+
+
+class ProjectCategories(BaseModel):
+    categories: list[ProjectCategory]
+
+
+class ProjectBase(BaseModel):
     title: str
     description: str
     text: str
-    project_category_id: int
     created_at: date
-    preview_photo_id: int
 
 
-class Projects(ProjectsBase):
+class Project(ProjectBase):
     id: int
     is_posted: bool
     is_posted_in_bot: bool
+    preview_photo: Media
+    project_category: ProjectCategory
 
 
-class ProjectsCreate(ProjectsBase):
+class Projects(BaseModel):
+    projects: list[Project]
+
+
+class ProjectCreate(ProjectBase):
     is_posted: bool = True
     is_posted_in_bot: bool = False
+    preview_photo_id: int
+    project_category_id: int
 
 
-class ProjectsUpdate(BaseModel):
+class ProjectUpdate(BaseModel):
     title: str | None = None
     description: str | None = None
     text: str | None = None
