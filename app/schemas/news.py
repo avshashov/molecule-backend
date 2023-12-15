171f860d36406f117a0600f41e0f2821ea5bd768
avshashov <tauquantum@gmail.com>
2023-12-15 15:02:37 +1000

    feat: added preview_photo for News schema and created NewsMany schema

--- app/schemas/news.py
+++ app/schemas/news.py
@@ -2,23 +2,30 @@ from datetime import date
 
 from pydantic import BaseModel
 
+from app.schemas.media import Media
+
 
 class NewsBase(BaseModel):
     description: str
     text: str
     created_at: date
-    preview_photo_id: int
 
 
 class News(NewsBase):
     id: int
     is_posted: bool
     is_posted_in_bot: bool
+    preview_photo: Media
+
+
+class NewsMany(BaseModel):
+    news: list[News]
 
 
 class NewsCreate(NewsBase):
     is_posted: bool = True
     is_posted_in_bot: bool = False
+    preview_photo_id: int
 
 
 class NewsUpdate(BaseModel):
