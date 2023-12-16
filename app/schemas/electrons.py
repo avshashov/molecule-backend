000a9d486875b4bab8e001081681269c50f2a45b
avshashov <tauquantum@gmail.com>
2023-12-16 14:14:01 +1000

    feat: splitted Electron and Elctrons schemas, updated schema attributes

--- app/schemas/electrons.py
+++ app/schemas/electrons.py
@@ -1,18 +1,24 @@
 from pydantic import BaseModel
 
+from app.schemas.media import Media
+
 
 class ElectronsBase(BaseModel):
     name: str
     description: str
-    photo_id: int
 
 
 class ElectronsCreate(ElectronsBase):
-    pass
+    photo_id: int
 
 
-class Electrons(ElectronsCreate):
+class Electron(ElectronsBase):
     id: int
+    preview_photo: Media
+
+
+class Electrons(BaseModel):
+    participants: list[Electron]
 
 
 class ElectronsUpdate(BaseModel):
