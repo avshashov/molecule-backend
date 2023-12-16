a477ae48ae33f503016d605abf40f9209689a5e6
avshashov <tauquantum@gmail.com>
2023-12-16 14:32:11 +1000

    feat: added new schemas for Media api and new attributes

--- app/schemas/media.py
+++ app/schemas/media.py
@@ -1,21 +1,34 @@
 from pydantic import BaseModel
 
 
+class MediaType(BaseModel):
+    id: int
+    name: str
+
+
+class MediaTypes(BaseModel):
+    types: list[MediaType]
+
+
 class MediaBase(BaseModel):
     name: str
-    type_id: int
     description: str
 
 
 class MediaCreate(MediaBase):
+    type_id: int
     link: str
 
 
-class Media(MediaCreate):
+class Media(MediaBase):
     id: int
+    media_type: MediaType
+
+
+class MediaMany(BaseModel):
+    media: list[Media]
 
 
 class MediaUpdate(BaseModel):
     name: str | None = None
     description: str | None = None
-
