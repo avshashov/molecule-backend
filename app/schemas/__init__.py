d8eb0a4698ca7bd4082700f972e213d0759e9497
avshashov <tauquantum@gmail.com>
2023-12-16 14:32:34 +1000

    feat: added new imports for Media

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from app.schemas.dates import Dates
 from app.schemas.electrons import Electrons, ElectronsCreate, ElectronsUpdate
-from app.schemas.media import Media, MediaCreate, MediaUpdate
+from app.schemas.media import Media, MediaMany, MediaCreate, MediaUpdate, MediaTypes
 from app.schemas.news import News, NewsCreate, NewsMany, NewsUpdate
 from app.schemas.press import Press, PressCreate, PressUpdate, PressMany
 from app.schemas.projects import Project, ProjectCategories, ProjectCreate, Projects, ProjectUpdate
