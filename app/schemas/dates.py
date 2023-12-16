4cdf9bda5994ef2e440040bfb86b8526f0df4c35
avshashov <tauquantum@gmail.com>
2023-12-16 15:01:28 +1000

    feat: added Dates schema field documentation

--- app/schemas/dates.py
+++ app/schemas/dates.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Dates(BaseModel):
-    years: list
-
+    years: list[int] = Field(
+        title='List items dates',
+        description='List unique dates of available elements in YEAR format',
+        examples=[[2022, 2023, 2024]]
+    )
