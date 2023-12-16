07f1627082e1de70ccb46627b216e7df70d77452
avshashov <tauquantum@gmail.com>
2023-12-16 13:25:56 +1000

    refactor: isort

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,6 +1,6 @@
-from app.schemas.press import Press, PressUpdate, PressCreate
-from app.schemas.news import News, NewsUpdate, NewsCreate, NewsMany
-from app.schemas.projects import Project, Projects, ProjectUpdate, ProjectCreate, ProjectCategories
-from app.schemas.media import Media, MediaCreate
 from app.schemas.dates import Dates
 from app.schemas.electrons import Electrons, ElectronsCreate, ElectronsUpdate
+from app.schemas.media import Media, MediaCreate
+from app.schemas.news import News, NewsCreate, NewsMany, NewsUpdate
+from app.schemas.press import Press, PressCreate, PressUpdate
+from app.schemas.projects import Project, ProjectCategories, ProjectCreate, Projects, ProjectUpdate
