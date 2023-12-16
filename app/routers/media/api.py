7e48e78578bfd79fdc6de7d0836de3b3d3eeb0be
avshashov <tauquantum@gmail.com>
2023-12-16 14:32:55 +1000

    feat: added response models for Media api

--- app/routers/media/api.py
+++ app/routers/media/api.py
@@ -15,10 +15,10 @@ Session = Annotated[AsyncSession, Depends(database.get_session)]
 
 
 @router.get('/media_types')
-async def get_media_types(db: Session):
+async def get_media_types(db: Session) -> schemas.MediaTypes:
     types = await CRUD.get_types(session=db)
     if types:
-        return types
+        return {'types': types}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'MediaTypes not found')
 
 
@@ -27,18 +27,18 @@ async def get_media_files(
     db: Session,
     count: Annotated[int, Query(ge=5, le=50)] = 10,
     offset: Annotated[int, Query(ge=0, le=50)] = 0,
-):
+) -> schemas.MediaMany:
     media = await CRUD.get_items(session=db, count=count, offset=offset)
     if media:
-        return media
+        return {'media': media}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Media not found')
 
 
 @router.get('/{media_id}')
-async def get_media_file(db: Session, media_id: int):
+async def get_media_file(db: Session, media_id: int) -> schemas.Media:
     media = await CRUD.get_items(session=db, id=media_id)
     if media:
-        return media
+        return media[0]
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Media with {media_id} not found')
 
 
@@ -49,7 +49,7 @@ async def upload_media_file(
     media_name: Annotated[str, Form()],
     media_type_id: Annotated[int, Form()],
     media_description: Annotated[str, Form()],
-):
+) -> schemas.MediaCreate:
     filepath = await files.save_file(file, media_name)
     media_metadata = schemas.MediaCreate(
         name=media_name, type_id=media_type_id, link=filepath, description=media_description
