3d4fde13ef6770eb7335185ce1bca3b1f1e7e31d
avshashov <tauquantum@gmail.com>
2023-12-16 14:15:20 +1000

    feat: added PressMany import

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -2,5 +2,5 @@ from app.schemas.dates import Dates
 from app.schemas.electrons import Electrons, ElectronsCreate, ElectronsUpdate
 from app.schemas.media import Media, MediaCreate, MediaUpdate
 from app.schemas.news import News, NewsCreate, NewsMany, NewsUpdate
-from app.schemas.press import Press, PressCreate, PressUpdate
+from app.schemas.press import Press, PressCreate, PressUpdate, PressMany
 from app.schemas.projects import Project, ProjectCategories, ProjectCreate, Projects, ProjectUpdate
