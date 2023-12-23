05729633822bac3c5e1e3b3d48e78447302a4847
avshashov <tauquantum@gmail.com>
2023-12-23 21:18:40 +1000

    fix: added schemas documentation for Electrons

--- app/schemas/electrons.py
+++ app/schemas/electrons.py
@@ -1,27 +1,55 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
-from app.schemas.media import Media
+from app.schemas.media import MediaPreviewPhoto
 
 
 class ElectronsBase(BaseModel):
-    name: str
-    description: str
+    name: str = Field(
+        title='Participant\'s full name',
+        description='Full name of the participant in the electrons section',
+        examples=['Test user'],
+    )
+    description: str = Field(
+        title='Participant\'s description',
+        description='Description of the participant in the electrons section in markdown format',
+        examples=['The man who created the magnificent [picture](url_to_picture)'],
+    )
 
 
 class ElectronsCreate(ElectronsBase):
-    photo_id: int
+    photo_id: int = Field(
+        title='Photo ID', description='Photo ID of the participant on the section page', examples=[1], gt=0
+    )
 
 
 class Electron(ElectronsBase):
-    id: int
-    preview_photo: Media
+    id: int = Field(title='Participant ID', description='Participant ID', examples=[1], gt=0)
+    preview_photo: MediaPreviewPhoto
 
 
 class Electrons(BaseModel):
-    participants: list[Electron]
+    participants: list[Electron] = Field(
+        title='Participants list', description='List of all members of the electrons section'
+    )
 
 
 class ElectronsUpdate(BaseModel):
-    name: str | None = None
-    description: str | None = None
-    photo_id: int | None = None
+    name: str | None = Field(
+        default=None,
+        title='New participant\'s full name',
+        description='New full name of the participant in the electrons section',
+        examples=['Test user'],
+    )
+    description: str | None = Field(
+        default=None,
+        title='New participant\'s description',
+        description='New description of the participant in the electrons section in markdown format',
+        examples=['The man who created the magnificent [picture](url_to_picture)'],
+    )
+    photo_id: int | None = Field(
+        default=None,
+        title='New photo ID',
+        description='New photo ID of the participant on the section page',
+        examples=[1],
+        gt=0,
+    )
