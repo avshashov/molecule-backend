d15ba4a905d9cb657213dbfe89f6fb8e66dd4726
avshashov <tauquantum@gmail.com>
2023-12-23 22:33:54 +1000

    fix: added schemas documentation for media

--- app/schemas/media.py
+++ app/schemas/media.py
@@ -1,38 +1,110 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class MediaType(BaseModel):
-    id: int
-    name: str
+    id: int = Field(title='Media type ID', description='Media type ID', examples=[1], gt=0)
+    name: str = Field(title='Media type name', description='Media type name', examples=['photo'])
 
 
 class MediaTypes(BaseModel):
-    types: list[MediaType]
+    types: list[MediaType] = Field(
+        title='List of media types',
+        description='List of media types',
+        examples=[[{"id": 1, "name": "photo"}, {"id": 2, "name": "video"}]],
+    )
 
 
 class MediaBase(BaseModel):
-    name: str
-    description: str
+    name: str = Field(
+        title='Media name',
+        description='Media file name',
+        examples=['Some photo name'],
+    )
+    description: str = Field(
+        title='Media description',
+        description='Media file description',
+        examples=['Some photo description'],
+    )
 
 
 class MediaCreate(MediaBase):
-    type_id: int
-    link: str
+    type_id: int = Field(
+        title='Type ID',
+        description='Type ID',
+        examples=['1'],
+        gt=0,
+    )
+    link: str = Field(
+        title='Link to media',
+        description='Link to media file',
+        examples=['https://somepath-to-media/some-media.png'],
+    )
 
 
 class Media(MediaBase):
-    id: int
+    id: int = Field(
+        title='Media ID',
+        description='Media file ID',
+        examples=['1'],
+        gt=0,
+    )
+    link: str = Field(
+        title='Link to media',
+        description='Link to media file',
+        examples=['https://somepath-to-media/some-media.png'],
+    )
     media_type: MediaType
 
 
 class MediaPreviewPhoto(MediaBase):
-    id: int
+    id: int = Field(
+        title='Preview Photo ID',
+        description='Preview Photo file ID',
+        examples=['1'],
+        gt=0,
+    )
+    link: str = Field(
+        title='Link to preview photo',
+        description='Link to preview photo file',
+        examples=['https://somepath-to-media/some-media.png'],
+    )
 
 
 class MediaMany(BaseModel):
-    media: list[Media]
+    media: list[Media] = Field(
+        title='Media list',
+        description='List of media files',
+        examples=[
+            [
+                {
+                    "name": "Some photo name",
+                    "description": "Some photo description",
+                    "id": 1,
+                    "link": "https://somepath-to-media/some-media-photo.png",
+                    "media_type": {"id": 1, "name": "photo"},
+                },
+                {
+                    "name": "Some video name",
+                    "description": "Some video description",
+                    "id": 1,
+                    "link": "https://somepath-to-media/some-media-video.mp4",
+                    "media_type": {"id": 2, "name": "video"},
+                },
+            ]
+        ],
+    )
 
 
 class MediaUpdate(BaseModel):
-    name: str | None = None
-    description: str | None = None
+    name: str | None = Field(
+        default=None,
+        title='New media name',
+        description='New media file name',
+        examples=['Some photo name'],
+    )
+    description: str | None = Field(
+        default=None,
+        title='New media description',
+        description='New media file description',
+        examples=['Some photo description'],
+    )
