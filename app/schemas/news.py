5d93337ea5f957c3848481b2cac79ec0c4db3b9f
avshashov <tauquantum@gmail.com>
2023-12-25 12:46:03 +1000

    feat: added schemas documentation for news

--- app/schemas/news.py
+++ app/schemas/news.py
@@ -1,37 +1,143 @@
 from datetime import date
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from app.schemas.media import MediaPreviewPhoto
 
 
 class NewsBase(BaseModel):
-    description: str
-    text: str
-    created_at: date
+    description: str = Field(
+        title='News description',
+        description='Brief description of the news',
+        examples=[
+            'The shooting of paintings and artists continues. '
+            'Today we filmed the first part of our promo.'
+        ],
+    )
+    text: str = Field(
+        title='News text',
+        description='Text of the news in the news section in markdown format',
+        examples=[
+            'Aggressive luxury is trying to trample the Molecule '
+            'already at the entrance... [picture](url_to_picture)'
+        ],
+    )
+    created_at: date = Field(
+        title='News release date',
+        description='News release date',
+        examples=['2023-12-24'],
+    )
 
 
 class News(NewsBase):
-    id: int
-    is_posted: bool
-    is_posted_in_bot: bool
+    id: int = Field(
+        title='News ID',
+        description='News ID',
+        examples=['1'],
+        gt=0,
+    )
+    is_posted: bool = Field(
+        title='Publication of news',
+        description='Has the news been published?',
+        examples=[True],
+    )
+    is_posted_in_bot: bool = Field(
+        title='Publication news in a telegram bot',
+        description='Has the news been published in a telegram bot?',
+        examples=[False],
+    )
     preview_photo: MediaPreviewPhoto
 
 
 class NewsMany(BaseModel):
-    news: list[News]
+    news: list[News] = Field(
+        title='News list',
+        description='News list',
+        examples=[
+            [
+                {
+                    "description": "The shooting of paintings and artists continues. "
+                                   "Today we filmed the first part of our promo.",
+                    "text": "Aggressive luxury is trying to trample the Molecule "
+                            "already at the entrance... [picture](url_to_picture)",
+                    "created_at": "2023-12-24",
+                    "id": 1,
+                    "is_posted": True,
+                    "is_posted_in_bot": False,
+                    "preview_photo": {
+                        "name": "Some photo name",
+                        "description": "Some photo description",
+                        "id": 1,
+                        "link": "https://somepath-to-media/some-media.png",
+                    },
+                }
+            ]
+        ],
+    )
 
 
 class NewsCreate(NewsBase):
-    is_posted: bool = True
-    is_posted_in_bot: bool = False
-    preview_photo_id: int
+    is_posted: bool = Field(
+        default=True,
+        title='Publication of news',
+        description='Has the news been published?',
+        examples=[True],
+    )
+    is_posted_in_bot: bool = Field(
+        default=False,
+        title='Publication news in a telegram bot',
+        description='Has the news been published in a telegram bot?',
+        examples=[False],
+    )
+    preview_photo_id: int = Field(
+        title='New preview photo ID',
+        description='New preview photo ID news',
+        examples=[1],
+        gt=0,
+    )
 
 
 class NewsUpdate(BaseModel):
-    description: str | None = None
-    text: str | None = None
-    created_at: date | None = None
-    is_posted: bool = True
-    preview_photo_id: int | None = None
-    is_posted_in_bot: bool = False
+    description: str | None = Field(
+        default=None,
+        title='New news description',
+        description='New brief description of the news',
+        examples=[
+            'The shooting of paintings and artists continues. '
+            'Today we filmed the first part of our promo.',
+        ],
+    )
+    text: str | None = Field(
+        default=None,
+        title='New news text',
+        description='New text of the news in the news section in markdown format',
+        examples=[
+            'Aggressive luxury is trying to trample the Molecule '
+            'already at the entrance... [picture](url_to_picture)',
+        ],
+    )
+    created_at: date | None = Field(
+        default=None,
+        title='New news release date',
+        description='New news release date',
+        examples=['2023-12-24'],
+    )
+    is_posted: bool = Field(
+        default=True,
+        title='Publication of news',
+        description='Has the news been published?',
+        examples=[True],
+    )
+    preview_photo_id: int | None = Field(
+        default=None,
+        title='New preview photo ID',
+        description='New preview photo ID news',
+        examples=[1],
+        gt=0,
+    )
+    is_posted_in_bot: bool = Field(
+        default=False,
+        title='Publication news in a telegram bot',
+        description='Has the news been published in a telegram bot?',
+        examples=[False],
+    )
